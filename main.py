05a67aaa553697c446ad8c5c113a0f0743144d20
Shan-JCB <71997263@continental.edu.pe>
2024-04-03 18:22:47 -0500

    Linea base del proyecto

---
+++ main.py
@@ -0,0 +1,19 @@
+def mcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def ingresar_numero_positivo(mensaje):
+    while True:
+        try:
+            num = int(input(mensaje))
+            if num <= 0:
+                raise ValueError("Por favor, ingresa un número natural positivo.")
+            return num
+        except ValueError as e:
+            print(e)
+
+num1 = ingresar_numero_positivo("Ingresa el primer número: ")
+num2 = ingresar_numero_positivo("Ingresa el segundo número: ")
+
+print("El máximo común divisor de", num1, "y", num2, "es:", mcd(num1, num2))
